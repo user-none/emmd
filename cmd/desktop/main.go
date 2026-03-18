60e0aad0940d86461ae31b7cb72dbcdf883d390a
John Schember <john@nachtimwald.com>
2026-03-17 21:48:24 -0400

    add missing desktop runner

---
+++ cmd/desktop/main.go
@@ -0,0 +1,36 @@
+//go:build !libretro && !ios
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/user-none/eblitui/desktop"
+	"github.com/user-none/emmd/adapter"
+)
+
+func main() {
+	romPath := flag.String("rom", "", "path to ROM file (opens UI if not provided)")
+	sixButton := flag.Bool("six-button", true, "enable 6-button controller")
+	flag.Parse()
+
+	factory := &adapter.Factory{}
+
+	if *romPath != "" {
+		options := map[string]string{}
+		if *sixButton {
+			options["six_button"] = "true"
+		} else {
+			options["six_button"] = "false"
+		}
+		if err := desktop.RunDirect(factory, *romPath, options, nil); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if err := desktop.Run(factory); err != nil {
+		log.Fatal(err)
+	}
+}
